ee0d796025883451e873b519a1c23e7e8bcd0dbe
Justin Lapidus <44097524+justinlapidus@users.noreply.github.com>
2020-11-08 16:11:52 -0500

    added ability to change and create new excel files

---
+++ calculators/calculation.js
@@ -0,0 +1,30 @@
+const xlsx = require("xlsx");
+
+let wb = xlsx.readFile("Key_Production_Calculator_v2.xlsx", {cellDates:true});
+
+let ws = wb.Sheets["Calculator"];
+
+let customerName, projectName; 
+
+customerName = 'Justin'
+projectName = 'yooooo'
+
+
+ws["D4"] = {
+    t: 's',
+    v: customerName,
+    r: '<t>Customer Name</t>',
+    h: customerName,
+    w: customerName
+}
+
+ws["I4"] = {
+    t: 's',
+    v: projectName,
+    r: '<t>Project Name</t>',
+    h: projectName,
+    w: projectName
+}
+
+
+xlsx.writeFile(wb, "New.xlsx");
