975d4ec1b63532e981be0c18cdb209f27e1e00de
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2020-12-02 06:37:57 -0500

    Clearing current routes to test for deployment

--- server/server.js
+++ server/server.js
@@ -1,48 +1,18 @@
-import path from 'path';
 import express from 'express';
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
-import config from './config/config.js'
-import drillRouter from './routes/drillRouter.js';
-import {connectToDatabase} from './connectMongodb.js';
-
-//connect to database
-const db = connectToDatabase().on(
-   "error",
-   console.error.bind(console, "MongoDB connection error:")
- );
- db.once("open", () => {
-   console.log("Successfully connected to mongoose database!");
-   
- });
-
-//initialize app
 const app = express();
 
-//enable request logging for development debugging
-app.use(morgan('dev'));
+const PORT = process.env.PORT || 5000;
 
-//body parsing middleware
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+// configure body parser for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.json());
-
-/* serve static files - see http://expressjs.com/en/starter/static-files.html */
-// app.use('/', express.static('./../../client'));
-
-/* Request Handler for route /api/drills
-*/
-app.use('/api/drills/', drillRouter);
-
-/* Request handler for all other routes
-   Sends a response (res) to go to the homepage for all routes not specified */
-app.all('/*', (req, res) => {
-
-
-   res.statusCode === 404 ? res.send('Sorry, information not available') : res.sendFile(path.resolve('../client/public/index.html'))
-        
+// routes
+app.get('/', (req, res) => {
+	res.send('Hello from MERN');
 });
 
-app.listen(config.port, () => console.log(`App now listening on port ${config.port}`));
+// Bootstrap server
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}.`);
+});
