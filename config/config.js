2d46dddab283f116d30d60b95e691aaf0c30d967
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2020-12-02 20:14:54 -0500

    Testing file location change on deployment

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+      uri: "mongodb+srv://SandvikAdmin:SandAdminPW4123680@sandvikwebapp.2isge.mongodb.net/DrillDB?retryWrites=true&w=majority", //place the URI of your mongo database here.
+  }
+};
