969d9f915d87ccb2dc2ab6621c4ca8875035adc6
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2020-12-02 22:17:01 -0500

    Testing Deployment

--- server/config/express.js
+++ server/config/express.js
@@ -28,6 +28,7 @@ module.exports.init = () => {
     // add a router
     app.use('/api/example', exampleRouter);
 
+        if (process.env.NODE_ENV === 'production') {
         // Serve any static files
         app.use(express.static(path.join(__dirname, '../../client/build')));
 
@@ -35,6 +36,7 @@ module.exports.init = () => {
         app.get('*', function(req, res) {
             res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
         });
+    }
 
 
     return app
