6712b3dfe51f4117fbc3d8f6dd9cdd1036adf8dc
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2020-12-02 22:00:52 -0500

    Revert "Attempt to connect by removing mongo connection."
    This reverts commit 24512eadced8541ad58f69d7e49efc09f98db39e.

--- server/config/express.js
+++ server/config/express.js
@@ -1,5 +1,6 @@
 const path = require('path'),
     express = require('express'),
+    mongoose = require('mongoose'),
     morgan = require('morgan'),
     bodyParser = require('body-parser'),
     exampleRouter = require('../routes/examples.server.routes');
@@ -9,6 +10,11 @@ module.exports.init = () => {
         connect to database
         - reference README for db uri
     */
+    mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
+        useNewUrlParser: true
+    });
+    mongoose.set('useCreateIndex', true);
+    mongoose.set('useFindAndModify', false);
 
     // initialize app
     const app = express();
@@ -22,13 +28,13 @@ module.exports.init = () => {
     // add a router
     app.use('/api/example', exampleRouter);
 
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../../client/build')));
+        // Serve any static files
+        app.use(express.static(path.join(__dirname, '../../client/build')));
 
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-    });
+        // Handle React routing, return all requests to React app
+        app.get('*', function(req, res) {
+            res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+        });
 
 
     return app
