74d3986e4714ae453b756443f7cb84bbec029fec
Joha2001 <57204571+Joha2001@users.noreply.github.com>
2020-12-06 16:04:15 -0500

    Changes from merge
    Should be able to deploy with Kevin's changes now.

--- client/src/views/Calc/calc.js
+++ client/src/views/Calc/calc.js
@@ -3,23 +3,19 @@ import './calc.css'
 import NavBar from '../../components/Header/NavBar'
 import { Redirect } from 'react-router-dom';
 import isLoggedIn from '../../helpers/is_logged_in';
-const Input = (props) => {
+const Input = () => {
+    
     const [input, setInput] = useState({})
     const [output, setOutput] = useState({})
     const [submitting, setSubmitting] = useState(true);
     const toggle = () => { setSubmitting(!submitting) };
 
-    let Bit;
-    let Burden;
-    let Spacing;
-    let Bench;
-    let Sub_Drilling;
-    let Density;
-    let penRate;
-    let meterDrilled;
-    let targetUtil;
-    let targetProdHour;
-    let targetProdTon;
+    let RigModel = "Rig 1";
+    let HoleDiameter;
+    let HoleDepth;
+    let Material;
+    let Elevation;
+    let AmbientTemp;
     let inSchema = {};
 
     let RotaryDrill;
@@ -28,24 +24,32 @@ const Input = (props) => {
 
     function handleIn() {
         inSchema = {
-            bit: Bit,
-            bur: Burden,
-            spa: Spacing,
-            ben: Bench,
-            sub: Sub_Drilling,
-            den: Density,
-            pen: penRate,
-            met: meterDrilled,
-            util: targetUtil,
-            proHour: targetProdHour,
-            proTon: targetProdTon,
-
+            Rig: RigModel,
+            Dia: HoleDiameter,
+            Dep: HoleDepth,
+            Mat: Material,
+            Ele: Elevation,
+            Tem: AmbientTemp,
         }
     }
 
     function handleOut() {
-        RotaryDrill = 'Test';
-        Savings = 12345;
+        if (input.Rig === "Rig 1") {
+            RotaryDrill = 'D254S';
+            Savings = 750000;
+        }
+        else if (input.Rig === "Rig 2") {
+            RotaryDrill = 'D55SP';
+            Savings = 1000000;
+        }
+        else if (input.Rig === "Rig 3") {
+            RotaryDrill = '1190E';
+            Savings = 500000;
+        }
+        else {
+            RotaryDrill = 'D75KX';
+            Savings = 1250000;
+        }
 
         outSchema = {
             Drill: RotaryDrill,
@@ -61,99 +65,69 @@ const Input = (props) => {
     }
     if (!isLoggedIn()) {
         return <Redirect to="/Login" />;
-    }
+      }
     return (
-        <div >
-            <div className="calc-navbar">
-                <NavBar></NavBar>
-            </div>
+        <div>
+            <NavBar></NavBar>
             <header className="calc-header">{submitting ? 'Inputs' : 'Results'}</header>
-            <div className="calc-form">
-                {submitting &&
-                    <form id="Parameter-List" onSubmit={() => { handleIn(); handleOut(); handleSubmit(); }}>
-                        BIT (mm):
-                <input className="calc-input"
-                            type="number"
-                            onChange={(e) => { Bit = e.target.value }}
-                            required />
-                        <p>
-                            Burden (m):
-                <input className="calc-input"
-                                type="number"
-                                onChange={(e) => { Burden = e.target.value }}
-                                required />
-                        </p>
-                Spacing (m):
-                <input className="calc-input"
-                            type="number"
-                            onChange={(e) => { Spacing = e.target.value }}
-                            required />
-                        <p>
-                            Bench (m):
-                <input className="calc-input"
-                                type="number"
-                                onChange={(e) => { Bench = e.target.value }}
-                                required />
-                        </p>
-                Sub-Drilling (m):
-                <input className="calc-input"
-                            type="number"
-                            onChange={(e) => { Sub_Drilling = e.target.value }}
-                            required />
-                        <p>
-                            Rock Density (UCS):
-            <input className="calc-input"
-                                type="number"
-                                onChange={(e) => { Density = e.target.value }}
-                                required />
-                        </p>
-                    Current Pen Rate (m/hr):
-                <input className="calc-input"
-                            type="number"
-                            onChange={(e) => { penRate = e.target.value }}
-                            required />
-                        <p>
-                            Meter Drilled per Shift (m):
-            <input className="calc-input"
-                                type="number"
-                                onChange={(e) => { meterDrilled = e.target.value }}
-                                required />
-                        </p>
-                    Target Utilization (%):
-                <input className="calc-input"
-                            type="number"
-                            onChange={(e) => { targetUtil = e.target.value }}
-                            required />
-                        <p>
-                            Target Production Hours per Month (hr):
-            <input className="calc-input"
-                                type="number"
-                                onChange={(e) => { targetProdHour = e.target.value }}
-                                required />
-                        </p>
-                    Target Production Tonnage per Month (ton):
-                <input className="calc-input"
-                            type="number"
-                            onChange={(e) => { targetProdTon = e.target.value }}
-                            required />
-                        <p>
-                            <input className="calc-button" type="submit" name="Submit"></input>
-                        </p>
-                    </form>}
-
-                {!submitting &&
-                    <div>
-                        <div>
-                            Your Paramter List: {input.bit}, {input.bur}m, {input.spa}m
+            {submitting &&
+                <form id="Parameter-List" onSubmit={() => { handleIn(); handleOut(); handleSubmit(); }}>
+                    <label>
+                        Pick your rig model:
+                        <select onChange={(e) => { RigModel = e.target.value }}>
+                            <option value="Rig 1">Rig 1</option>
+                            <option value="Rig 2">Rig 2</option>
+                            <option value="Rig 3">Rig 3</option>
+                            <option value="Rig 4">Rig 4</option>
+                        </select>
+                    </label>
                     <p>
-                                Your Rotary Drill: {output.Drill}
-                            </p>
+                        Hole Diameter (m):
+                <input
+                            type="number"
+                            onChange={(e) => { HoleDiameter = e.target.value }}
+                            required />
+                    </p>
+                Hole Depth (m):
+                <input
+                        type="number"
+                        onChange={(e) => { HoleDepth = e.target.value }}
+                        required />
+                    <p>
+                        Material Type:
+                <input
+                            type="text"
+                            onChange={(e) => { Material = e.target.value }}
+                            required />
+                    </p>
+                Elevation (m):
+                <input
+                        type="number"
+                        onChange={(e) => { Elevation = e.target.value }}
+                        required />
+                    <p>
+                        Ambient Temperature (C):
+            <input
+                            type="number"
+                            onChange={(e) => { AmbientTemp = e.target.value }}
+                            required />
+                    </p>
+                    <input type="submit" name="Submit"></input>
+                </form>}
+
+            {!submitting &&
+                <div>
+                    <div>
+                        Your Paramter List: {input.Rig}, {input.Dia}m, {input.Dep}m, {input.Mat}, {input.Ele}m, {input.Tem}C
+                    <p>
+                            Your Rotary Drill: {output.Drill}
+                        </p>
                     Your Savings: ${output.Sav}
-                        </div>
-                        <button className="calc-button" onClick={() => toggle()}>Return</button>
                     </div>
-                } </div>
-        </div >
+                    <button onClick={() => toggle()}>Return</button>
+                </div>
+            }
+        </div>
     );
 
 }
